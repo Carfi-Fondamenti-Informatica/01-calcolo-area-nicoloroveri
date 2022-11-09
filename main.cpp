987c80f6be948922cc33db7390a925cbd24af295
nicoloroveri <116441124+nicoloroveri@users.noreply.github.com>
2022-11-09 15:03:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,8 @@ using namespace std;
 int main(){
 float a = 0, b = 0, c = 0; 
 cin >> a >> b >> c;
-float area triangolo = 0, area quadrato = 0, area rettangolo = 0, area trapezio = 0;
-area triangolo = ((a * b) / 2);
-area quadrato = (a * a);
-area rettangolo = (a * b);
-area trapezio = (((a + b) * c) / 2);
-cout << area triangolo <<endl;
-cout << area quadrato <<endl;
-cout << area rettangolo <<endl;
-cout << area trapezio <<endl;                 
-return 0;                 
+cout << ((a * b) / 2) <<endl; //area triangolo
+cout << (a * a) <<endl; //area quadrato
+cout << (a * b) <<endl; //area rettangolo
+cout << (((a + b) * c) / 2) <<endl; //area trapezio                                 
 }
